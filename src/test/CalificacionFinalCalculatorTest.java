889f296da7d96b47d98003a1c35ed480c0b716fe
Rafael <zs21017266@estudiantes.uv.mx>
2024-10-14 18:16:51 -0600

    Codigo terminado

--- src/test/CalificacionFinalCalculatorTest.java
+++ src/test/CalificacionFinalCalculatorTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.Test;
 
@@ -17,4 +18,49 @@ public class CalificacionFinalCalculatorTest {
         assertEquals(0 , CalificacionFinalCalculator.calcularCalificacionFinal(0, 0, 0), 0.01);
     }
     
+    @Test
+    public void Caso3() {
+        assertEquals(10, CalificacionFinalCalculator.calcularCalificacionFinal(10, 10, 10), 0.01);
+    }
+
+    @Test
+    public void Caso4() {
+        assertEquals(6.33, CalificacionFinalCalculator.calcularCalificacionFinal(4.5, 6.5, 8), 0.01);
+    }
+
+    @Test
+    public void Caso5() {
+        assertThrows(ArithmeticException.class, () -> {
+            CalificacionFinalCalculator.calcularCalificacionFinal(-1.0, 5, 7);
+        });
+    }
+
+    @Test
+    public void Caso6() {
+        assertThrows(ArithmeticException.class, () -> {
+            CalificacionFinalCalculator.calcularCalificacionFinal(5, 11, 7);
+        });
+    }
+
+    @Test
+    public void Caso7   () {
+        assertThrows(ArithmeticException.class, () -> {
+            CalificacionFinalCalculator.calcularCalificacionFinal(5, 5, 10.1);
+        });
+    }
+
+    @Test
+    public void Caso8() {
+        assertEquals(4.4, CalificacionFinalCalculator.calcularCalificacionFinal(3.3, 4.4, 5.5), 0.01);
+    }
+
+    @Test
+    public void Caso9() {
+        assertEquals(8.4, CalificacionFinalCalculator.calcularCalificacionFinal(9.0, 8.7, 7.5), 0.01);
+    }
+
+    @Test
+    public void Caso10() {
+        assertEquals(6.0, CalificacionFinalCalculator.calcularCalificacionFinal(6.0, 6.0, 6.0), 0.01);
+    }
 }
